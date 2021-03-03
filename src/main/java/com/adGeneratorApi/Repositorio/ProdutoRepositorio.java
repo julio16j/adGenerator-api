21e716f7b9c09ee64356cbdb5c1ab5127a38c849
Bruno <brunoob14540@gmail.com>
2021-03-03 16:06:16 -0300

    query de produto

--- src/main/java/com/adGeneratorApi/Repositorio/ProdutoRepositorio.java
+++ src/main/java/com/adGeneratorApi/Repositorio/ProdutoRepositorio.java
@@ -1,12 +1,20 @@
 package com.adGeneratorApi.Repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.adGeneratorApi.Dominio.Entidade.Produto;
+import com.adGeneratorApi.Dominio.Enum.CategoriaProduto;
 
 @Repository
 public interface ProdutoRepositorio extends JpaRepository<Produto, String> {
-	
-
+	@Query("SELECT p FROM Produto p "
+			+ "WHERE (:nome IS NULL OR p.nome LIKE :nome || '%') "
+			+ "AND (:descricao IS NULL OR p.descricao LIKE :descricao || '%') "
+			+ "AND (:categoria IS NULL OR p.categoria = :categoria)")
+	public List<Produto> findByFilters(@Param("nome") String nome, @Param("descricao") String descricao, @Param("categoria") CategoriaProduto categoria);
 }
